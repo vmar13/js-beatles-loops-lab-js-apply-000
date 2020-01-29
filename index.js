b2d6816170882e68c258eaa6e689308172fb4e79
Vanessa Martinez <vmar76@gmail.com>
2020-01-29 02:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array=[];
   for (var i=0; i<musicians.length; i++) {
-    array.push(musicians[i] + plays guitar`;
-  
+    array.push(musicians[i] + ' plays ' + instruments[i]);
+  return array;
 }
