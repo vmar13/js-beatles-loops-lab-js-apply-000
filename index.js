0ec6e4a1c24a73b1a3975c3da7ee702a11ba10c6
Vanessa Martinez <vmar76@gmail.com>
2020-01-29 02:25:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
-  var i=[];
-  for (i=0; i<n; i++) {
+  var array=[];
+  for (var i=0; i<n; i++) {
     return `John Lennon plays guitar`;
   }
   while (i.push) 
